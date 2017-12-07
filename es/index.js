ad3a605dba7fe23ef980bc4285118af4ebc65b00
heifade <heifade@126.com>
2017-12-07 18:21:51 +0800

    1

--- es/index.js
+++
@@ -1,9 +0,0 @@
-import { isInteger, CheckIntegerParams } from "./check/checkInteger";
-import { isFloat, CheckFloatParams } from "./check/checkFloat";
-import { isReal, CheckRealParams } from "./check/checkReal";
-import { isEmail, CheckEmailParams } from "./check/checkEmail";
-import { isPhoneNumber, CheckPhoneNumberParams } from "./check/checkPhoneNumber";
-import { isFixedTelephoneNumber, CheckFixedTelephoneNumberParams } from "./check/checkFixedTelephoneNumber";
-import { isIDCardNo, CheckIDCardNoParams } from "./check/checkIDCardNo";
-export { isInteger, CheckIntegerParams, isFloat, CheckFloatParams, isReal, CheckRealParams, isEmail, CheckEmailParams, isPhoneNumber, CheckPhoneNumberParams, isFixedTelephoneNumber, CheckFixedTelephoneNumberParams, isIDCardNo, CheckIDCardNoParams };
-//# sourceMappingURL=index.js.map
