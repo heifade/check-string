a9f4125547481314c87cb9454b673e6d259d8f4d
heifade <heifade@126.com>
2018-10-24 21:49:52 +0800

    no message

--- build.config.ts
+++ build.config.ts
@@ -1,18 +1,21 @@
 import { BuildConfig } from "happywork-node-builder";
 
 const config: BuildConfig = {
-  input: "src/index.ts",
+  input: {
+    'check-string-es': "src/index.ts",
+    // 'check-string-cjs': "src/index.ts",
+  },
   output: [
     {
       dir: "dist",
-      file: "check-string-es.js",
-      format: "es"
-    },
-    {
-      dir: "dist",
-      file: "check-string-cjs.js",
+      // file: "check-string-es.js",
       format: "cjs"
-    }
+    },
+    // {
+    //   dir: "dist",
+    //   // file: "check-string-cjs.js",
+    //   format: "cjs"
+    // }
   ],
   external: [],
   mini: false
